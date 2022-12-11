6349e03fe4263fee3700ab4554305c8185147f64
mumarualfajar <kuhaaku@gmail.com>
2022-12-12 01:45:17 +0700

    first commit: splash & onboarding screen

---
+++ app/src/main/java/com/mumaralfajar/movie/onboarding/OnboardingThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.mumaralfajar.movie.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mumaralfajar.movie.R
+
+class OnboardingThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_three)
+    }
+}
---
+++ app/src/main/java/com/mumaralfajar/movie/onboarding/OnboardingTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.mumaralfajar.movie.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mumaralfajar.movie.R
+
+class OnboardingTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_two)
+    }
+}
